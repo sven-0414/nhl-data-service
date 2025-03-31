b374ed0099d121126ff9c414e0ebd9e8f7115025
sven414 <svenpleriksson@gmail.com>
2025-03-31 16:27:53 +0200

    Initial commit

---
+++ src/main/java/se/sven/nhldataservice/NhlDataServiceApplication.java
@@ -0,0 +1,13 @@
+package se.sven.nhldataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhlDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/sven/nhldataservice/NhlDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sven.nhldataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
