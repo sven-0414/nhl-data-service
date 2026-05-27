20f976b5e8d8d5fd6f16288d03b643a1b288f21f
sven-0414 <sveneriksson@icloud.com>
2026-05-27 15:27:13 +0200

    Updating configuration and documentation

--- src/main/java/se/sven/nhldataservice/config/JwtAuthenticationFilter.java
+++ src/main/java/se/sven/nhldataservice/config/JwtAuthenticationFilter.java
@@ -72,6 +72,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
-        }        filterChain.doFilter(request, response);
+        }
+        filterChain.doFilter(request, response);
     }
 }
